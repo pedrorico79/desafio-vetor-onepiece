23150d5f224c792bd2c24fb2225fe092bb31ffc8
fsFernando072 <fernando.fsilva@sptech.school>
2025-06-05 11:13:07 -0300

    Adicionando armadilhas e premios

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
+const armadilhas = [];
+const premios = [];
+
 function iniciarJogo() {
     let secInicio = document.querySelector('.inicio');
     secInicio.style.display = 'none';
 
+    let dificuldade = document.querySelector('#dificuldade').value;
+
     let tabela = document.querySelector('table');
     let frase = '';
     let inicio = 1;
+
+    while (armadilhas.length < dificuldade || premios.length < dificuldade) {
+        let errado = Number((Math.random() * (100 - 1) + 1).toFixed());
+        let certo  = Number((Math.random() * (100 - 1) + 1).toFixed());
+
+        if (!armadilhas.includes(errado) && !premios.includes(errado) && armadilhas.length < dificuldade) {
+            armadilhas.push(errado);
+        }
+
+        if (!armadilhas.includes(certo) && !premios.includes(certo) && premios.length < dificuldade) {
+            premios.push(certo);
+        }
+    }
     
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
         frase += `<tr>`;
-        for (let j = 0; j < 20; j++) {
+        for (let j = 0; j < 10; j++) {
             frase += `<td onclick="exibir(${inicio})" id="td${inicio}"></td>`;
+
             inicio++;
         }
         frase += `</tr>`;
     }
 
+    console.log(premios, armadilhas)
     tabela.innerHTML = frase;
 }
 
 function exibir(indice) {
     let td = document.querySelector(`#td${indice}`);
 
+    if (armadilhas.includes(indice)) {
+        td.style.backgroundColor = `#bb0000`;
+    }
+    
+    if (premios.includes(indice)) {
+        td.style.backgroundColor = `#00bb00`;
+    }
+    
     td.innerHTML = indice;
 }
