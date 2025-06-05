25d8959fcff3178d077edc1c8bb36542e22c37d8
fsFernando072 <fernando.fsilva@sptech.school>
2025-06-05 10:32:11 -0300

    subindo mapa inicial

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function iniciarJogo() {
+    let secInicio = document.querySelector('.inicio');
+    secInicio.style.display = 'none';
+
     let tabela = document.querySelector('table');
     let frase = '';
+    let inicio = 1;
     
     for (let i = 0; i < 20; i++) {
         frase += `<tr>`;
         for (let j = 0; j < 20; j++) {
-            
+            frase += `<td onclick="exibir(${inicio})" id="td${inicio}"></td>`;
+            inicio++;
         }
+        frase += `</tr>`;
     }
+
+    tabela.innerHTML = frase;
+}
+
+function exibir(indice) {
+    let td = document.querySelector(`#td${indice}`);
+
+    td.innerHTML = indice;
 }
