f1ac65157f3e5c9ff569a49b96c0585f741e0e12
fsFernando072 <fernando.fsilva@sptech.school>
2025-06-05 10:12:06 -0300

    Adicionando primeiro html

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarJogo() {
+    let tabela = document.querySelector('table');
+    let frase = '';
+    
+    for (let i = 0; i < 20; i++) {
+        frase += `<tr>`;
+        for (let j = 0; j < 20; j++) {
+            
+        }
+    }
+}
